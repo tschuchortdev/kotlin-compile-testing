271a827e05d2ea1e525bbd925e52e9e8e572bd37
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-04-26 21:10:37 +0200

    Make Kotlin upgrade easier

--- src/test/kotlin/com/tschuchort/compiletesting/KotlinCompilationTests.kt
+++ src/test/kotlin/com/tschuchort/compiletesting/KotlinCompilationTests.kt
@@ -632,7 +632,7 @@ class KotlinCompilationTests {
 	fun `detects the plugin provided for compilation via pluginClasspaths property`() {
 		val result = defaultCompilerConfig().apply {
 			sources = listOf(SourceFile.kotlin("kSource.kt", "class KSource"))
-			pluginClasspaths = listOf(classpathOf("kotlin-scripting-compiler-1.3.72"))
+			pluginClasspaths = listOf(classpathOf("kotlin-scripting-compiler-${System.getProperty("KOTLIN_VERSION")}"))
 		}.compile()
 
 		assertThat(result.exitCode).isEqualTo(ExitCode.OK)
