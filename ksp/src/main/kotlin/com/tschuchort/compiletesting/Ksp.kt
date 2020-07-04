c5ee366968f2299f09ea15a5e1a2fe7d2b3ae49e
Yigit Boyar <yboyar@gmail.com>
2020-07-04 10:08:35 -0700

    Use a custom registrar to invoke KSP
    This commit gets rid of classpath generation and instead just uses a custom registrar
    to register and implementation of  where
    plugins are pre-defined.
    
    I've also changed the symbol processors to be a list similar to KAPT and also added an
    extension property to get the folder where generated KSP code is kept

--- ksp/src/main/kotlin/com/tschuchort/compiletesting/Ksp.kt
+++ ksp/src/main/kotlin/com/tschuchort/compiletesting/Ksp.kt
@@ -3,66 +3,96 @@
  */
 package com.tschuchort.compiletesting
 
-import org.jetbrains.kotlin.ksp.KotlinSymbolProcessingCommandLineProcessor
-import org.jetbrains.kotlin.ksp.KotlinSymbolProcessingComponentRegistrar
+import org.jetbrains.kotlin.com.intellij.mock.MockProject
+import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.ksp.AbstractKotlinSymbolProcessingExtension
+import org.jetbrains.kotlin.ksp.KspOptions
 import org.jetbrains.kotlin.ksp.processing.SymbolProcessor
+import org.jetbrains.kotlin.resolve.jvm.extensions.AnalysisHandlerExtension
+import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstanceOrNull
 import java.io.File
 
-private const val KSP_PLUGIN_ID = "org.jetbrains.kotlin.ksp"
-
-private fun KotlinCompilation.initAndGetKspConfig(): KspConfiguration {
-    val config = KspConfiguration(workingDir.resolve("ksp"))
-    if (config.workingDir.exists()) {
-        // already configured, just return
-        return config
+/**
+ * The list of symbol processors for the kotlin compilation.
+ * https://goo.gle/ksp
+ */
+var KotlinCompilation.symbolProcessors: List<SymbolProcessor>
+    get() = getKspRegistrar().processors
+    set(value) {
+        val registrar = getKspRegistrar()
+        registrar.processors = value
     }
-    config.classesOutDir.mkdirs()
-    config.sourcesOurDir.mkdirs()
-    config.syntheticSources.mkdirs()
-    val kspOptions = listOf(
-        PluginOption(KSP_PLUGIN_ID, "apclasspath", config.syntheticSources.path),
-        PluginOption(KSP_PLUGIN_ID, "classes", config.classesOutDir.path),
-        PluginOption(KSP_PLUGIN_ID, "sources", config.sourcesOurDir.path)
-    )
-    compilerPlugins = compilerPlugins + KotlinSymbolProcessingComponentRegistrar()
-    commandLineProcessors = commandLineProcessors + listOf(KotlinSymbolProcessingCommandLineProcessor())
-    pluginOptions = pluginOptions + kspOptions
-    return config
+
+/**
+ * The directory where generates KSP sources are written
+ */
+val KotlinCompilation.kspSourcesDir: File
+    get() = kspWorkingDir.resolve("sources")
+
+/**
+ * The working directory for KSP
+ */
+private val KotlinCompilation.kspWorkingDir: File
+    get() = workingDir.resolve("ksp")
+
+/**
+ * The directory where compiled KSP classes are written
+ */
+// TODO this seems to be ignored by KSP and it is putting classes into regular classes directory
+//  but we still need to provide it in the KSP options builder as it is required
+//  once it works, we should make the property public.
+private val KotlinCompilation.kspClassesDir: File
+    get() = kspWorkingDir.resolve("classes")
+
+/**
+ * Custom subclass of [AbstractKotlinSymbolProcessingExtension] where processors are pre-defined instead of being
+ * loaded via ServiceLocator.
+ */
+private class KspTestExtension(
+    options: KspOptions,
+    private val processors: List<SymbolProcessor>
+) : AbstractKotlinSymbolProcessingExtension(
+    options = options,
+    testMode = false
+) {
+    override fun loadProcessors() = processors
 }
 
-fun KotlinCompilation.symbolProcessors(
-    vararg processors: Class<out SymbolProcessor>
-) {
-    check(processors.isNotEmpty()) {
-        "Must provide at least 1 symbol processor"
-    }
-    val config = initAndGetKspConfig()
-
-    // create a fake classpath that references our symbol processor
-    config.syntheticSources.apply {
-        resolve("META-INF/services/org.jetbrains.kotlin.ksp.processing.SymbolProcessor").apply {
-            parentFile.mkdirs()
-            // keep existing ones in case this function is called multiple times
-            val existing = if (exists()) {
-                readLines(Charsets.UTF_8)
-            } else {
-                emptyList()
-            }
-            val processorNames = existing + processors.map {
-                it.typeName
-            }
-            writeText(
-                processorNames.joinToString(System.lineSeparator())
-            )
+/**
+ * Registers the [KspTestExtension] to load the given list of processors.
+ */
+private class KspCompileTestingComponentRegistrar(
+    private val compilation: KotlinCompilation
+) : ComponentRegistrar {
+    var processors = emptyList<SymbolProcessor>()
+    override fun registerProjectComponents(project: MockProject, configuration: CompilerConfiguration) {
+        if (processors.isEmpty()) {
+            return
         }
+        val options = KspOptions.Builder().apply {
+            this.classesOutputDir = compilation.kspClassesDir.also {
+                it.deleteRecursively()
+                it.mkdirs()
+            }
+            this.sourcesOutputDir = compilation.kspSourcesDir.also {
+                it.deleteRecursively()
+                it.mkdirs()
+            }
+        }.build()
+        val registrar = KspTestExtension(options, processors)
+        AnalysisHandlerExtension.registerExtension(project, registrar)
     }
-    this.kaptSourceDir
 }
 
-private data class KspConfiguration(
-    val workingDir: File
-) {
-    val classesOutDir = workingDir.resolve("classesOutput")
-    val sourcesOurDir = workingDir.resolve("sourcesOutput")
-    val syntheticSources = workingDir.resolve("synthetic-ksp-service")
+/**
+ * Gets the test registrar from the plugin list or adds if it does not exist.
+ */
+private fun KotlinCompilation.getKspRegistrar(): KspCompileTestingComponentRegistrar {
+    compilerPlugins.firstIsInstanceOrNull<KspCompileTestingComponentRegistrar>()?.let {
+        return it
+    }
+    val kspRegistrar = KspCompileTestingComponentRegistrar(this)
+    compilerPlugins = compilerPlugins + kspRegistrar
+    return kspRegistrar
 }
--- ksp/src/test/kotlin/com/tschuchort/compiletesting/AbstractTestSymbolProcessor.kt
+++ ksp/src/test/kotlin/com/tschuchort/compiletesting/AbstractTestSymbolProcessor.kt
@@ -4,7 +4,10 @@ import org.jetbrains.kotlin.ksp.processing.CodeGenerator
 import org.jetbrains.kotlin.ksp.processing.Resolver
 import org.jetbrains.kotlin.ksp.processing.SymbolProcessor
 
-internal open class AbstractSymbolProcessor : SymbolProcessor {
+/**
+ * Helper class to write tests, only used in Ksp Compile Testing tests, not a public API.
+ */
+internal open class AbstractTestSymbolProcessor : SymbolProcessor {
     protected lateinit var codeGenerator: CodeGenerator
     override fun finish() {
     }
